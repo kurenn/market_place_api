20622f17cb6ed5843f6516d49bf3980dd5176b73
Abraham Kuri <kurenn@icalialabs.com>
2014-06-20 00:00:31 -0500

    Adds api constraints to the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+require 'api_contraints'
+
 MarketPlaceApi::Application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
-    scope module: :v1 do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
     end
   end
