80b4cb6a4fa644d5747d28862f68e9c9f8a339c2
Abraham Kuri <kurenn@icalialabs.com>
2014-06-21 21:23:45 -0500

    Adds user firsts specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,8 @@
 
 FactoryGirl.define do
   factory :user do
+    email { Faker::Internet.email }
+    password "12345678"
+    password_confirmation "12345678"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.build(:user) }
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
 end
