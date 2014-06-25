2034ddf8ce8eda655ef16d8e2a92181b59e046a8
Abraham Kuri <kurenn@icalialabs.com>
2014-06-24 23:42:06 -0500

    Adds destroy action to the users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -89,4 +89,14 @@ describe Api::V1::UsersController do
       it { should respond_with 422 }
     end
   end
+
+  describe "DELETE #destroy" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      delete :destroy, { id: @user.id }, format: :json
+    end
+
+    it { should respond_with 204 }
+
+  end
 end
