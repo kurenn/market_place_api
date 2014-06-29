36693d2e649836364b2ef496404a839d45edd7e2
Abraham Kuri <kurenn@icalialabs.com>
2014-06-29 14:23:42 -0500

    Fixes the global before hook to just run on controller specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,7 +44,7 @@ RSpec.configure do |config|
   config.include Request::JsonHelpers, :type => :controller
   config.include Request::HeadersHelpers, :type => :controller
 
-  config.before(:each) do
-    include_default_accept_headers 
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
   end
 end
