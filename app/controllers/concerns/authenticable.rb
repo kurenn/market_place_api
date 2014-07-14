4d3d21890614a522f122469c3640a3738a70da2b
Abraham Kuri <kurenn@icalialabs.com>
2014-07-14 11:26:18 -0500

    Adds user_signed_in? method to know whether the user is logged in or not

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -6,7 +6,11 @@ module Authenticable
   end
 
   def authenticate_with_token!
-    render json: { errors: "Not authenticated" }, status: :unauthorized unless current_user.present?
+    render json: { errors: "Not authenticated" }, status: :unauthorized unless user_signed_in?
+  end
+
+  def user_signed_in?
+    current_user.present? 
   end
   
 end
--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -34,4 +34,24 @@ describe Authenticable do
 
     it {  should respond_with 401 }
   end
+
+  describe "#user_signed_in?" do
+    context "when there is a user on 'session'" do
+      before do
+        @user = FactoryGirl.create :user
+        authentication.stub(:current_user).and_return(@user)
+      end
+
+      it { should be_user_signed_in }
+    end
+
+    context "when there is no user on 'session'" do
+      before do
+        @user = FactoryGirl.create :user
+        authentication.stub(:current_user).and_return(nil)
+      end
+
+      it { should_not be_user_signed_in }
+    end
+  end
 end
