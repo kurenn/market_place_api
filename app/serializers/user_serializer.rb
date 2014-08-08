6342601c9b8c951c5bb20810a978b2f196cf6403
Abraham Kuri <kurenn@icalialabs.com>
2014-08-08 13:43:59 -0500

    Fixes the sessions controller spec

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :created_at, :updated_at
+  attributes :id, :email, :created_at, :updated_at, :auth_token
 end
--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -17,7 +17,7 @@ describe Api::V1::SessionsController do
 
       it "returns the user record corresponding to the given credentials" do
         @user.reload
-        expect(json_response[:auth_token]).to eql @user.auth_token
+        expect(json_response[:user][:auth_token]).to eql @user.auth_token
       end
 
       it { should respond_with 200 }
