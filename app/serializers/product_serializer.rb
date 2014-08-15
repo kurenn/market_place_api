e99ddf4dd56706f27563bb53b5db9a393bda577d
Abraham Kuri <kurenn@icalialabs.com>
2014-08-15 14:45:47 -0500

    Embeds the user into the product json output

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,4 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :title, :price, :published
+  has_one :user
 end
--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -13,6 +13,11 @@ describe Api::V1::ProductsController do
       expect(product_response[:title]).to eql @product.title
     end
 
+    it "has the user as a embeded object" do
+      product_response = json_response[:product]
+      expect(product_response[:user][:email]).to eql @product.user.email
+    end
+
     it { should respond_with 200 }
   end
 
@@ -25,6 +30,13 @@ describe Api::V1::ProductsController do
     it "returns 4 records from the database" do
       products_response = json_response
       expect(products_response[:products]).to have(4).items
+    end
+
+    it "returns the user object into each product" do
+      products_response = json_response[:products]
+      products_response.each do |product_response|
+        expect(product_response[:user]).to be_present
+      end
     end
 
     it { should respond_with 200 }
