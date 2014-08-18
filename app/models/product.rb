3e9495b6ff51a77018b84afc8046943aee259513
Abraham Kuri <kurenn@icalialabs.com>
2014-08-18 16:12:02 -0500

    Adds search scopes on the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,20 @@ class Product < ActiveRecord::Base
 
   belongs_to :user
 
+  scope :filter_by_title, lambda { |keyword|
+    where("lower(title) LIKE ?", "%#{keyword.downcase}%" ) 
+  }
+
+  scope :above_or_equal_to_price, lambda { |price| 
+    where("price >= ?", price) 
+  }
+
+  scope :below_or_equal_to_price, lambda { |price| 
+    where("price <= ?", price) 
+  }
+
+  scope :recent, -> {
+    order(:updated_at)
+  }
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -17,4 +17,66 @@ describe Product do
 
   it { should belong_to :user }
 
+  describe ".filter_by_title" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, title: "A plasma TV"
+      @product2 = FactoryGirl.create :product, title: "Fastest Laptop"
+      @product3 = FactoryGirl.create :product, title: "CD player"
+      @product4 = FactoryGirl.create :product, title: "LCD TV"
+    end
+
+    context "when a 'TV' title pattern is sent" do
+      it "returns the 2 products matching" do
+        expect(Product.filter_by_title("TV")).to have(2).items
+      end
+
+      it "returns the products matching" do
+        expect(Product.filter_by_title("TV").sort).to match_array([@product1, @product4])
+      end
+    end
+  end
+
+  describe ".above_or_equal_to_price" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, price: 100
+      @product2 = FactoryGirl.create :product, price: 50
+      @product3 = FactoryGirl.create :product, price: 150
+      @product4 = FactoryGirl.create :product, price: 99
+    end
+
+    it "returns the products which are above or equal to the price" do
+      expect(Product.above_or_equal_to_price(100).sort).to match_array([@product1, @product3])
+    end
+  end
+
+  describe ".below_or_equal_to_price" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, price: 100
+      @product2 = FactoryGirl.create :product, price: 50
+      @product3 = FactoryGirl.create :product, price: 150
+      @product4 = FactoryGirl.create :product, price: 99
+    end
+
+    it "returns the products which are above or equal to the price" do
+      expect(Product.below_or_equal_to_price(99).sort).to match_array([@product2, @product4])
+    end
+  end
+
+  describe ".recent" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, price: 100
+      @product2 = FactoryGirl.create :product, price: 50
+      @product3 = FactoryGirl.create :product, price: 150
+      @product4 = FactoryGirl.create :product, price: 99
+
+      #we will touch some products to update them
+      @product2.touch
+      @product3.touch
+    end
+
+    it "returns the most updated records" do
+      expect(Product.recent).to match_array([@product3, @product2, @product4, @product1])
+    end
+  end
+  
 end
