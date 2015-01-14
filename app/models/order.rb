1f4891a87329fab9e74b05ff83c2fdb270432c20
Abraham Kuri <kurenn@icalialabs.com>
2015-01-14 15:57:29 -0600

    Updates the total calculation for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,7 +10,10 @@ class Order < ActiveRecord::Base
   before_validation :set_total!
 
   def set_total!
-    self.total = products.map(&:price).sum
+    self.total = 0
+    placements.each do |placement|
+      self.total += placement.product.price * placement.quantity
+    end
   end
 
   def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -18,11 +18,17 @@ describe Order do
       product_1 = FactoryGirl.create :product, price: 100
       product_2 = FactoryGirl.create :product, price: 85
 
-      @order = FactoryGirl.build :order, product_ids: [product_1.id, product_2.id]
+      placement_1 = FactoryGirl.build :placement, product: product_1, quantity: 3
+      placement_2 = FactoryGirl.build :placement, product: product_2, quantity: 15
+
+      @order = FactoryGirl.build :order
+
+      @order.placements << placement_1
+      @order.placements << placement_2
     end
 
     it "returns the total amount to pay for the products" do
-      expect{@order.set_total!}.to change{@order.total}.from(0).to(185)
+      expect{@order.set_total!}.to change{@order.total.to_f}.from(0).to(1575)
     end
   end
 
